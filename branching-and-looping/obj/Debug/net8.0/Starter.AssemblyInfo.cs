a7cd42d8e82034f24dc96e0438e1b387d7c766bc
Deep Patel <deepkalpana1@aol.com>
2024-06-02 01:45:15 -0400

    added suggested donation to pet details

--- branching-and-looping/obj/Debug/net8.0/Starter.AssemblyInfo.cs
+++ branching-and-looping/obj/Debug/net8.0/Starter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Starter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5da90a8b7a1c10ed0967ac985cf244b36b6a2dc9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28622bd210f8a92491cae1bc71850c48b360a5a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Starter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Starter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
